eed47dd8770c3b81c167ff47329ef47c1381aa67
연준 김 <kyjun050803@gmail.com>
2022-12-06 21:28:27 +0900

    스크롤바 추가 및 디자인 개선, 자료 링크 추가

--- main.py
+++ main.py
@@ -3,33 +3,43 @@ app = Flask(__name__)
 
 from NLP.dialoguehandler import DialogueHandler
 dlgHandler = DialogueHandler()
-botname = 'BeatusChatBot'
+botname = '어썸'
 
 def template(title, content, auto_submit = None):
     t = f'''<!doctype html>
+    
     <link rel="stylesheet" href="{ url_for('static', filename='design.css') }" media="(min-width:600px)">
     <html>
         <title>{title}</title>
         <header>
             <div id="logo">
                 <a href = "/">
-                    <img src="https://www.cnsa.hs.kr/images/hpw/common/logo.gif" alt="Logo" width="225" height="60" href="./index.html">
+                    <img src="https://www.cnsa.hs.kr/images/hpw/common/logo.gif" alt="Logo" width="225" height="55" href="./index.html">
                 </a>
             </div>
 
             <div id="top_menu">
-                <h2> 2022 Beatus 인공지능팀 프로젝트</h2>
+                <h2> AwSUM </h2>
                 <h4> KoBART와 TextRank를 이용한 학습용 챗봇 </h4>
             </div>
         </header>
         
-        <article id="content">
+        <article id="content" style="
+    overflow-y: scroll;
+    height: calc(100vh - 240px);
+">
             {content}
         </article>
         
-        <footer>
-            <p>Made by 김연준, 홍준혁</p>
+        <footer style="
+    position: fixed;
+    bottom: 0;
+">
+            <p>2022 Beatus</p>
+            <p>AI Team | 2학년 김연준, 홍준혁</p>
+            <p>Web Team | 1학년 조인성</p>
         </footer>
+        <script>document.querySelector("article").scroll(0,99999999)</script>
     '''
 
     if auto_submit:
@@ -44,16 +54,32 @@ def getTextForm(username, dlg_state):
     form = ''
     if dlg_state < 100: # 사용자에게 채팅 입력을 받을 때
         form = f'''
-            <form class = "input" name = "getUserText" action="/chat/" method="POST">
-                <input type="textarea" name="usertext" placeholder="채팅 입력"></textarea>
+            <form class = "input" name = "getUserText" action="/chat/" method="POST" style="
+    position: fixed;
+    bottom: 90px;
+    width: 96%;
+    height: 30px;
+    padding: .5% 1%;
+    display: flex;
+">
+                <input type="textarea" name="usertext" placeholder="채팅 입력" style="
+    flex: 1; autofocus/">
                 <input type="hidden" name="current_username" value={username}>
                 <input type="hidden" name="dlg_state" value={dlg_state}>
                 <input type="submit" value="입력">
             </form>'''
     else: # 챗봇이 계속 말할 때 (입력받는 것 없이 form 제출)
         form = f'''
-            <form class = "input" name = "getUserText" action="/chat/" method="POST">
-                <input type="textarea" name="usertext" placeholder="채팅 입력"></textarea>
+            <form class = "input" name = "getUserText" action="/chat/" method="POST" style="
+    position: fixed;
+    bottom: 90px;
+    width: 96%;
+    height: 30px;
+    padding: .5% 1%;
+    display: flex;
+">
+                <input type="textarea" name="usertext" placeholder="채팅 입력" style="
+    flex: 1;" readonly/>
                 <input type="hidden" name="current_username" value={username}>
                 <input type="hidden" name="dlg_state" value={dlg_state}>
             </form>
@@ -89,6 +115,10 @@ def home():
                 <input type="submit" value="완료">
             </form>
         </div>
+        <ul>
+            <h3><a href="/related-work/">텍스트 요약 원리</a></h3>
+            <h3><a href="https://github.com/Superfish83/Beatus_NLP_Project_2">GitHub</a></h3>
+        </ul>
     '''
     return template('Main Page', getName)
 
@@ -110,7 +140,9 @@ def chat():
                 return '다른 이름을 설정하세요!'
 
             #채팅 내역
-            chat_history[username] = getTextTemplate(botname, f'안녕, {username}!')
+            chat_history[username] = getTextTemplate(botname, f'''안녕, {username}!
+            "설명 시작"을 입력하고 나에게 설명하려는 내용을 입력한 뒤
+            "설명 끝"을 입력하면 요약해 줄게.''')
             chatting += chat_history[username]
             
             #채팅 입력 form
@@ -141,6 +173,10 @@ def chat():
 
     elif request.method == 'GET':
         return 'ERROR'
+
+@app.route('/related-work/')
+def related_work():
+    return render_template('related_work.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
